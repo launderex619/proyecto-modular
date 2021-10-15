99159b4836cf1abca2fdca4441e45826931c3c9a
Luis Medina <luis.m.medina@oracle.com>
2021-10-14 21:52:04 -0500

    mate

--- project/controllers/testing_controller.py
+++ project/controllers/testing_controller.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as m
 import cv2 as cv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -23,20 +24,44 @@ class TestingController:
         img = cv.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
         return img
 
+    def rotate(self, phi, theta, psi):
+        phi = m.pi/2
+        theta = m.pi/4
+        psi = m.pi/2
+        print("phi =", phi)
+        print("theta  =", theta)
+        print("psi =", psi)
+        
+        R = Rz(psi) * Ry(theta) * Rx(phi)
+        print(np.round(R, decimals=2))
+        return R
+
     def run(self):      
         kps_act = obtenerKeypoints(1)
         kps_ant = obtenerKeypoints(2)
 
         # Extrayendo X y Y de el keypoint
-        kp_xy_act = np.array([np.array([kp[0][0][0] for kp in kps_act]), 
-                            np.array([kp[0][0][1] for kp in kps_act])])
+        kp_xy_act = np.array([np.array([kp[0][0] for kp in kps_act]), 
+                            np.array([kp[0][1] for kp in kps_act])])
 
-        kp_xy_ant = np.array([np.array([kp['pt'][0] for kp in kps_ant]),
-                            np.array([kp['pt'][1] for kp in kps_ant])])      
+        kp_xy_ant = np.array([np.array([kp[0][0] for kp in kps_ant]), 
+                            np.array([kp[0][1] for kp in kps_ant])])    
 
         # obtenemos las matrices (la mate)
         matrizProyecion1 = None
         matrizProyecion2 = None
+
+
+        """
+        Since you already have the matched points form the image you can use findFundamentalMat() 
+            to get the fundamental matrix. Keep in mind you need at least 7 matched points to do this. 
+            If you have more then 8 points CV_FM_RANSAC might be the best option.
+
+        Then use cv::sfm::projectionsFromFundamental() to find the projection matrix for each image, 
+            check if the projection matrix is valid (ex.check if the points are in-front of the camera).
+
+        then feed the projections and the points it into cv::sfm::triangulatePoints().
+        """
 
         points_in_4d = cv.triangulatePoints(matrizProyecion1, matrizProyecion2, kp_xy_ant, kp_xy_act)
         points_in_3d = cv.convertPointsFromHomogeneous(points_in_4d.transpose())
@@ -44,6 +69,21 @@ class TestingController:
         # fig = plt.figure()
         # ax = fig.add_subplot(111, projection='3d')
         # ax.scatter([item[0][0] for item in points_in_3d], [item[0][1]])
+
+def Rx(theta):
+  return np.matrix([[ 1, 0           , 0           ],
+                   [ 0, m.cos(theta),-m.sin(theta)],
+                   [ 0, m.sin(theta), m.cos(theta)]])
+  
+def Ry(theta):
+  return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
+                   [ 0           , 1, 0           ],
+                   [-m.sin(theta), 0, m.cos(theta)]])
+  
+def Rz(theta):
+  return np.matrix([[ m.cos(theta), -m.sin(theta), 0 ],
+                   [ m.sin(theta), m.cos(theta) , 0 ],
+                   [ 0           , 0            , 1 ]])
 
 def obtenerKeypoints(numeroImagen):
     img = cv.imread(f"{config.PROJECT_PATH}/proyecto-modular/project/assets/photos/photo_mtrx{numeroImagen}.jpeg")
