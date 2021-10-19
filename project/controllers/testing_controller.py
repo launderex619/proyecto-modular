1e9d5ebfd6956c9e4131f0910c9b0e7aecda1539
Luis Medina <luis.m.medina@oracle.com>
2021-10-18 21:01:39 -0500

    Test triangulate "working"

--- project/controllers/testing_controller.py
+++ project/controllers/testing_controller.py
@@ -31,27 +31,27 @@ class TestingController:
         # print("phi =", phi)
         # print("theta  =", theta)
         # print("psi =", psi)
-        
+
         R = Rz(psi) * Ry(theta) * Rx(phi)
         # print(np.round(R, decimals=2))
         return R
 
     def translate(self, x0, y0, z0, x1, y1, z1):
-        difx = x1 - x0  
-        dify = y1 - y0  
-        difz = z1 - z0  
+        difx = x1 - x0
+        dify = y1 - y0
+        difz = z1 - z0
         return np.array([x0 + difx, y0 + dify, z0 + difz])
 
-    def run(self):      
+    def run(self):
         kps_act = obtenerKeypoints(1)
         kps_ant = obtenerKeypoints(2)
 
         # Extrayendo X y Y de el keypoint
-        kp_xy_act = np.array([np.array([kp[0][0] for kp in kps_act]), 
-                            np.array([kp[0][1] for kp in kps_act])])
+        kp_xy_act = np.array([np.array([kp[0][0] for kp in kps_act]),
+                              np.array([kp[0][1] for kp in kps_act])])
 
-        kp_xy_ant = np.array([np.array([kp[0][0] for kp in kps_ant]), 
-                            np.array([kp[0][1] for kp in kps_ant])])    
+        kp_xy_ant = np.array([np.array([kp[0][0] for kp in kps_ant]),
+                              np.array([kp[0][1] for kp in kps_ant])])
 
         # Angulos del dron euler
         R1 = self.rotate(0, 0, 0)
@@ -61,39 +61,74 @@ class TestingController:
         R2 = self.rotate(0, 0, 0)
         T2 = self.translate(0, 0, 0, 10, 0, 0)
 
-        peojection_mtx1 = np.array(np.concatenate(R1, T1)) 
-        peojection_mtx2 = np.array(np.concatenate(R2, T2))
+        """
 
-        points_in_4d = cv.triangulatePoints(peojection_mtx1, peojection_mtx2, kp_xy_ant, kp_xy_act)
-        points_in_3d = cv.convertPointsFromHomogeneous(points_in_4d.transpose())
+    
+    R = [[ 1 2 3 1],
+         [1 2 3 2],
+          [1 2 3 3]]
 
-        
+    T = [ 
+        1
+        2
+        3
+     ]
+        """
+        # rt1 = np.concatenate((R1, np.array([T1]).T), axis=1)
+        # rt2 = np.concatenate((R2, np.array([T2]).T), axis=1)
 
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
+        projection_mtx1 = np.array(
+            [[R1[0,0],R1[0,1],R1[0,2],T1[0]],
+            [R1[1,0],R1[1,1],R1[1,2],T1[1]],
+            [R1[2,0],R1[2,1],R1[2,2],T1[2]]]
+        )
+        projection_mtx2 = np.array(
+            [[R2[0,0],R2[0,1],R2[0,2],T2[0]],
+            [R2[1,0],R2[1,1],R2[1,2],T2[1]],
+            [R2[2,0],R2[2,1],R2[2,2],T2[2]]]
+        )
+
+        # falta multiplicar por la intrinseca
+
+        points_in_4d = cv.triangulatePoints(
+            projection_mtx1, projection_mtx2, kp_xy_ant, kp_xy_act)
+        # Normalize points [x, y, z, 1]
+        points_in_3d = points_in_4d / points_in_4d[3]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        # ax.scatter([p[0] for p in points_in_3d], [p[1] for p in points_in_3d], [p[2] for p in points_in_3d])
+        ax.scatter(points_in_3d[0], points_in_3d[1], points_in_3d[2])
         # ax.scatter([item[0][0] for item in points_in_3d], [item[0][1]])
 
+        print("ornelaseschido")
+
+
 def Rx(theta):
-  return np.matrix([[ 1, 0           , 0           ],
-                   [ 0, m.cos(theta),-m.sin(theta)],
-                   [ 0, m.sin(theta), m.cos(theta)]])
-  
+    return np.matrix([[1, 0, 0],
+                     [0, m.cos(theta), -m.sin(theta)],
+                     [0, m.sin(theta), m.cos(theta)]])
+
+
 def Ry(theta):
-  return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
-                   [ 0           , 1, 0           ],
-                   [-m.sin(theta), 0, m.cos(theta)]])
-  
+    return np.matrix([[m.cos(theta), 0, m.sin(theta)],
+                     [0, 1, 0],
+                     [-m.sin(theta), 0, m.cos(theta)]])
+
+
 def Rz(theta):
-  return np.matrix([[ m.cos(theta), -m.sin(theta), 0 ],
-                   [ m.sin(theta), m.cos(theta) , 0 ],
-                   [ 0           , 0            , 1 ]])
+    return np.matrix([[m.cos(theta), -m.sin(theta), 0],
+                     [m.sin(theta), m.cos(theta), 0],
+                     [0, 0, 1]])
+
 
 def obtenerKeypoints(numeroImagen):
-    img = cv.imread(f"{config.PROJECT_PATH}/proyecto-modular/project/assets/photos/photo_mtrx{numeroImagen}.jpeg")
+    img = cv.imread(
+        f"{config.PROJECT_PATH}/proyecto-modular/project/assets/photos/photo_mtrx{numeroImagen}.jpeg")
     gray = image_module.process_image(img)
-    img = image_module.resize_image(img, config.VIDEO_WITDH_RESIZE, config.VIDEO_HEIGHT_RESIZE)
+    img = image_module.resize_image(
+        img, config.VIDEO_WITDH_RESIZE, config.VIDEO_HEIGHT_RESIZE)
 
     # Find the chess board corners
     _, kp = cv.findChessboardCorners(gray, (9, 6), None)
     return kp
-        
